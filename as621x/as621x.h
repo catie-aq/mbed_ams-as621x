bd210a6845c9e096d737a40a59816fe2de54a3a8
Julien CHEVRIER <jchevrier@koncepto.io>
2021-11-19 17:06:23 +0100

    Add initial version of AS621x driver
    Signed-off-by: Julien CHEVRIER <jchevrier@koncepto.io>

--- as621x/as621x.h
+++ as621x/as621x.h
@@ -5,12 +5,68 @@
 #ifndef CATIE_SIXTRON_AS621X_H_
 #define CATIE_SIXTRON_AS621X_H_
 
+#include "mbed.h"
+
 namespace sixtron {
 
-class As621x
-{
+#define ALERT_SHIFT (5)
+#define ALERT_MASK (0x1)
+#define CONVRATE_SHIFT (6)
+#define CONVRATE_MASK (0x3)
+#define SLEEPMODE_SHIFT (8)
+#define SLEEPMODE_MASK (0x1)
+#define INTMODE_SHIFT (9)
+#define INTMODE_MASK (0x1)
+#define POLARITY_SHIFT (10)
+#define POLARITY_MASK (0x1)
+#define CFAULTS_SHIFT (11)
+#define CFAULTS_MASK (0x3)
+#define SINGLESHOT_SHIFT (15)
+#define SINGLESHOT_MASK (0x1)
+
+class As621x {
 public:
-    As621x();
+    enum class ErrorType : uint8_t {
+        Ok = 0,
+        I2cError = 1,
+        InvalidReg = 2,
+    };
+
+    enum class Add0Pin : uint8_t {
+        VSS = 0,
+        VDD = 1,
+        SDA = 2,
+        SCL = 3,
+    };
+    enum class Add1Pin : uint8_t {
+        SCL = 1,
+        VSS = 2,
+        PullUp_VDD = 3,
+    };
+
+    enum class RegisterAddress : uint8_t {
+        Tval = 0,
+        Config = 1,
+        TLow = 2,
+        THigh = 3,
+        Max = 4,
+    };
+    As621x(I2C *bus, Add1Pin add1, Add0Pin add0);
+
+    ErrorType read_config(uint16_t *value);
+    ErrorType write_config(uint16_t value);
+
+    ErrorType read_temperature(RegisterAddress reg, double *value);
+    ErrorType write_temperature(RegisterAddress reg, double value);
+
+private:
+    I2C *_bus;
+    uint8_t addr;
+    RegisterAddress last_reg;
+    bool alert_en;
+
+    ErrorType write_register(RegisterAddress reg, uint16_t value);
+    ErrorType read_register(RegisterAddress reg, uint16_t *value);
 };
 
 } // namespace sixtron
--- src/as621x.cpp
+++ src/as621x.cpp
@@ -4,10 +4,136 @@
  */
 #include "as621x/as621x.h"
 
+#define AS621X_BASE_ADDR (0x40)
+
+#define U16_TO_BYTE_ARRAY(u, ba)                                                                   \
+    do {                                                                                           \
+        (ba)[0] = (u >> 8) & 0xFF;                                                                 \
+        (ba)[1] = u & 0xFF;                                                                        \
+    } while (0)
+#define BYTE_ARRAY_TO_U16(ba, u)                                                                   \
+    do {                                                                                           \
+        u = ((ba)[0] << 8) | ((ba)[1]);                                                            \
+    } while (0)
+
 namespace sixtron {
 
-As621x::As621x()
+As621x::As621x(I2C *bus, Add1Pin add1, Add0Pin add0): _bus(bus)
 {
+    this->last_reg = RegisterAddress::Max;
+
+    assert((add1 >= Add1Pin::SCL) && (add1 <= Add1Pin::PullUp_VDD));
+    assert((add0 >= Add0Pin::VSS) && (add0 <= Add0Pin::SCL));
+
+    if (add1 == Add1Pin::PullUp_VDD) {
+        this->alert_en = true;
+
+        /* Add1 address bits are the same as ADD1_VSS. */
+        add1 = Add1Pin::VSS;
+    } else {
+        this->alert_en = false;
+    }
+
+    this->addr = (AS621X_BASE_ADDR | (static_cast<uint8_t>(add1) << 2)
+                         | (static_cast<uint8_t>(add0) << 0))
+            << 1;
+}
+
+As621x::ErrorType As621x::read_config(uint16_t *value)
+{
+    return this->read_register(RegisterAddress::Config, value);
+}
+
+As621x::ErrorType As621x::write_config(uint16_t value)
+{
+    return this->write_register(RegisterAddress::Config, value);
+}
+
+As621x::ErrorType As621x::read_temperature(RegisterAddress reg, double *value)
+{
+    ErrorType err;
+    uint16_t temp;
+
+    if (reg == RegisterAddress::Config) {
+        return ErrorType::InvalidReg;
+    }
+
+    err = this->read_register(reg, &temp);
+
+    if (err == ErrorType::Ok) {
+        *value = (double)(int16_t)temp / 128.0;
+    }
+
+    return err;
+}
+
+As621x::ErrorType As621x::write_temperature(RegisterAddress reg, double value)
+{
+    int16_t temp = round(value * 128);
+
+    if (reg == RegisterAddress::Config) {
+        return ErrorType::InvalidReg;
+    }
+
+    return this->write_register(reg, (uint16_t)temp);
+}
+
+As621x::ErrorType As621x::write_register(RegisterAddress reg, uint16_t value)
+{
+    ErrorType err = ErrorType::Ok;
+    char buf[3];
+
+    this->_bus->lock();
+
+    if (reg >= RegisterAddress::Max) {
+        err = ErrorType::InvalidReg;
+        goto write_reg_end;
+    }
+
+    U16_TO_BYTE_ARRAY(value, buf + 1);
+    buf[0] = static_cast<char>(reg);
+
+    if (this->_bus->write(this->addr, buf, 3)) {
+        err = ErrorType::I2cError;
+    }
+    this->last_reg = reg;
+
+write_reg_end:
+    this->_bus->unlock();
+    return err;
+}
+
+As621x::ErrorType As621x::read_register(RegisterAddress reg, uint16_t *value)
+{
+    ErrorType err = ErrorType::Ok;
+    char buf[2];
+    char addr = static_cast<char>(reg);
+    this->_bus->lock();
+
+    if (reg >= RegisterAddress::Max) {
+        err = ErrorType::InvalidReg;
+        goto read_reg_end;
+    }
+
+    if (this->last_reg != reg) {
+        if (this->_bus->write(this->addr, &addr, 1)) {
+            err = ErrorType::I2cError;
+            this->last_reg = RegisterAddress::Max;
+            goto read_reg_end;
+        }
+        this->last_reg = reg;
+    }
+
+    if (this->_bus->read(this->addr, buf, 2)) {
+        err = ErrorType::I2cError;
+        goto read_reg_end;
+    }
+
+    BYTE_ARRAY_TO_U16(buf, *value);
+
+read_reg_end:
+    this->_bus->unlock();
+    return err;
 }
 
 } // namespace sixtron
